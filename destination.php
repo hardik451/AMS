33df329db3cf07c1e4c0f21bed9a81f782ec9807
Hardik Rajbhandari <rajbhandarihardik@gmail.com>
2021-03-06 20:11:25 +0545

    updated

---
+++ destination.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+<title>Airlines Reservation System</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+
+<div class="bgded overlay" style="background-image:url(images/fsdf.jpg);"> 
+  <div class="wrapper row0">
+    <div id="topbar" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div class="fl_left">
+        <ul class="nospace">
+          <li><a href="#"><i class="fa fa-lg fa-home"></i></a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Login as Admin</a></li>
+        </ul>
+      </div>
+      <div class="fl_right">
+        <ul class="nospace">
+        <a href="#" class="btn btn-danger">Sign in</a>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="wrapper row1">
+    <header id="header" class="hoc clear"> 
+      <div id="logo" class="fl_left">
+        <h1><a href="#">Airlines Reservation System</a></h1>
+      </div>
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li><a href="index.php">Home</a></li>
+          <li class="active"><a href="destination.php">Destination</a></li>
+          <li><a class="drop" href="#">Book a Ticket</a>
+            <ul>
+              <li><a href="#">One Way</a></li>
+              <li><a href="#">Two Way</a></li>
+              <li><a href="#">Mountain Flights</a></li>
+            </ul>
+          </li>
+          <li><a class="drop" href="#">Service</a>
+           <ul>
+              <li><a href="#">Ground Support</a></li>
+              <li><a href="#">Economy Class</a></li>
+              <li><a href="#">Business Class</a></li>
+              <li><a href="#">Reservation</a></li>
+              <li><a href="#">Baggage</a></li>
+            </ul>
+            <li><a class="drop" href="#">Schedule</a>
+            <ul>
+              <li><a href="#">Domestic</a></li>
+              </ul>
+      </nav>
+    </header>
+  </div>
+    <div class="wrapper">
+    <div id="pageintro" class="hoc clear"> 
+       <div class="col-md-12 ">
+              <div class="row" id="main_contact_block">
+                                  <div class="contact_block ">
+                  <div class="contact_img">
+                                <a href="https://nepalairlines.com.np/home/package/delhi">
+          <img  src="https://nepalairlines.com.np/uploads/package/package_1452746682.jpg">
+          </a>
+                        </div>
+          <strong><a href="https://nepalairlines.com.np/home/package/delhi">Delhi </a></strong> 
+                    <br>
+                  <p><strong>Mr. Narayan Paudel</strong><br />
+Manager(Station Head), New Delhi<br />
+E-9, Connaught Place, New Delhi - 110001, India<br />
+Email: narayan.paudel2040@gmail.com, nac_del@nac.com.np<br />
+Mob: &lrm;&lrm;+919811530789</p>
+
+<p><strong>Mr. Madhav Lohani</strong><br />
+Airport Manager/Station Manager, New Delhi<br />
+Mob: +91 9811335813</p>
+
+<p><strong>Mr. Gopal Singh</strong><br />
+Airport Manager/Station Manager, New Delhi<br />
+Mob: +91 9811230706<br />
+&nbsp;<br />
+<strong>Mr. Rajendra Bahadur Shahi</strong><br />
+Finance Manager, New Delhi Station<br />
+Mob:+91 9811391208<br />
+Email: rbs.nac757@gmail.com, nac_fmdel@nac.com.np</p>
+
+<p><strong>Passenger Sales, Reservation and Flight Information</strong><br />
+Neha/Nandan/Sonu<br />
+Contact Numbers: +91 11 &lrm;43640497, 11 &lrm;43640498<br />
+Email :res.nac@bird.in</p>
+
+<p><strong>Cargo Sales</strong><br />
+Mr. Rajneesh Sharma<br />
+Mob:+91 9810255171<br />
+Email: rajneesh.sharma@birdtravels.com</p>
+                    <br>
+                  <a href="https://nepalairlines.com.np/home/package/delhi">View Details</a>
+
+                  </div>
+    </div>
+
+  </div>
+</div>
+  </main>
+</div>
+</div>
+</div>
+
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - Hardik Rajbhandari, Samit Napit, Sumana Baidar, Sewak Raj Joshi & Rakesh Patel</p>
+
+  </div>
+</div>
+
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+<title>Airlines Reservation System</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+
+<div class="bgded overlay" style="background-image:url(images/fsdf.jpg);"> 
+  <div class="wrapper row0">
+    <div id="topbar" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div class="fl_left">
+        <ul class="nospace">
+          <li><a href="#"><i class="fa fa-lg fa-home"></i></a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Login as Admin</a></li>
+        </ul>
+      </div>
+      <div class="fl_right">
+        <ul class="nospace">
+        <a href="login.php" class="btn btn-danger">Sign in</a>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="wrapper row1">
+    <header id="header" class="hoc clear"> 
+      <div id="logo" class="fl_left">
+        <h1><a href="#">Airlines Reservation System</a></h1>
+      </div>
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="login.php">Destination</a></li>
+          <li><a class="drop" href="#">Book a Ticket</a>
+            <ul>
+              <li><a href="#">One Way</a></li>
+              <li><a href="#">Two Way</a></li>
+              <li><a href="#">Mountain Flights</a></li>
+            </ul>
+          </li>
+          <li><a class="drop" href="#">Service</a>
+           <ul>
+              <li><a href="#">Ground Support</a></li>
+              <li><a href="#">Economy Class</a></li>
+              <li><a href="#">Business Class</a></li>
+              <li><a href="#">Reservation</a></li>
+              <li><a href="#">Baggage</a></li>
+            </ul>
+            <li><a class="drop" href="#">Schedule</a>
+            <ul>
+              <li><a href="#">Domestic</a></li>
+              </ul>
+      </nav>
+    </header>
+  </div>
+  <div class="wrapper">
+    <div id="pageintro" class="hoc clear"> 
+      <article>
+        <div>
+          <p class="heading">Airlines Reservation System</p>
+          <h2 class="heading">Airlines Reservation System</h2>
+          <p>Airlines Reservation System</p>
+        </div>
+        <footer>
+          <ul class="nospace inline pushright">
+            <li><a class="btn inverse" href="#">Learn More</a></li>
+            <li><a class="btn" href="login.php">Sign in</a></li>
+          </ul>
+        </footer>
+      </article>
+    </div>
+  </div>
+</div>
+  </main>
+</div>
+</div>
+</div>
+
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - Hardik Rajbhandari, Samit Napit, Sumana Baidar, Sewak Raj Joshi & Rakesh Patel</p>
+
+  </div>
+</div>
+
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
